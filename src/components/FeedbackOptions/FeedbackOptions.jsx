18372068d1d8187c06f9ed6fa890e88e42eaede0
Pavlo <p1kutsaev@gmail.com>
2023-03-26 15:54:51 +0300

    update

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import {
+  FeedbackOptionsStyled,
+  FeedbackOptionsBtnStyled,
+} from './FeedbackOptionsStyled';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <FeedbackOptionsStyled>
+      {options.map(option => {
+        return (
+          <li key={option}>
+            <FeedbackOptionsBtnStyled
+              onClick={() => {
+                onLeaveFeedback(option);
+              }}
+            >
+              {option}
+            </FeedbackOptionsBtnStyled>
+          </li>
+        );
+      })}
+    </FeedbackOptionsStyled>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import { StatisticStyled } from './Statistics.styled';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <StatisticStyled>
+      <div>
+        <p>
+          Good: <span>{good}</span>
+        </p>
+        <p>
+          Neutral: <span>{neutral}</span>
+        </p>
+        <p>
+          Bad: <span>{bad}</span>
+        </p>
+        <p>
+          Total: <span>{total}</span>
+        </p>
+        <p>
+          <span>{positivePercentage}%</span>
+        </p>
+      </div>
+    </StatisticStyled>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
